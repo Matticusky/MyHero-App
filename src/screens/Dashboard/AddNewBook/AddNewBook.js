fc676fd76325273fd1e540667ae9949ec4aa29bf
MalikSaadTanveer <maliksaadg777@gmail.com>
2025-01-30 18:49:56 +0500

    Image picker result change

--- src/screens/Dashboard/AddNewBook/AddNewBook.js
+++ src/screens/Dashboard/AddNewBook/AddNewBook.js
@@ -1,87 +1,88 @@
-import { Alert, StyleSheet, Text, View } from 'react-native'
-import styles from './styles'
-import { AssetsUploaderComponent, Button, CoverImagePicker, CustomizedInput, Header, ImagePicker, MainLayout, ScreenWrapper } from '../../../components'
-import { useState } from 'react';
-import { Icons } from '../../../assets';
-import { UtilityMethods } from '../../../utility';
+import {Alert, StyleSheet, Text, View} from 'react-native';
+import styles from './styles';
+import {
+  AssetsUploaderComponent,
+  Button,
+  CoverImagePicker,
+  CustomizedInput,
+  Header,
+  ImagePicker,
+  MainLayout,
+  ScreenWrapper,
+} from '../../../components';
+import {useState} from 'react';
+import {Icons} from '../../../assets';
+import {UtilityMethods} from '../../../utility';
 
-const AddNewBook = ({ navigation, route }) => {
-    const [coverImage, setCoverImage] = useState(null);
-    const [assets, setAssets] = useState([]);
-    const isEdited = route?.params;
+const AddNewBook = ({navigation, route}) => {
+  const [coverImage, setCoverImage] = useState(null);
+  const [assets, setAssets] = useState([]);
+  const isEdited = route?.params;
 
-    const [bookTitle, setBookTitle] = useState({
-        inputType: "text",
-        title: "Book Title",
-        value: "",
-        type: "text",
-        error: "",
-        placeholder: "Enter book title",
-        leftIcon: <Icons.BookIcon />
-    });
+  const [bookTitle, setBookTitle] = useState({
+    inputType: 'text',
+    title: 'Book Title',
+    value: '',
+    type: 'text',
+    error: '',
+    placeholder: 'Enter book title',
+    leftIcon: <Icons.BookIcon />,
+  });
 
-    const handleDiscard = () =>{
-        Alert.alert("Warning", "Are you sure you want to discard?",[
-            {
-                text:"No",
-            },
-            {
-                text:"Yes",
-                onPress: ()=>navigation.goBack()
-            }
-        ])
-    }
+  const handleDiscard = () => {
+    Alert.alert('Warning', 'Are you sure you want to discard?', [
+      {
+        text: 'No',
+      },
+      {
+        text: 'Yes',
+        onPress: () => navigation.goBack(),
+      },
+    ]);
+  };
 
+  return (
+    <MainLayout>
+      <Header
+        title={isEdited ? 'Edit book' : 'Add book'}
+        rightIcons={false}
+        showBackButton={true}
+      />
 
-    return (
-        <MainLayout>
-            <Header
-                title={isEdited ? "Edit book" : "Add book"}
-                rightIcons={false}
-                showBackButton={true}
-            />
+      <ScreenWrapper style={styles.cont}>
+        <CoverImagePicker
+          coverImage={coverImage}
+          setCoverImage={setCoverImage}
+        />
 
-            <ScreenWrapper style={styles.cont}>
+        <CustomizedInput
+          fieldInfo={bookTitle}
+          onChange={text => {
+            setBookTitle({...bookTitle, value: text, error: ''});
+          }}
+          style={styles.titleStyle}
+        />
 
-                <CoverImagePicker
-                    coverImage={coverImage}
-                    setCoverImage={setCoverImage}
-                />
+        <AssetsUploaderComponent assets={assets} setAssets={setAssets} />
 
-                <CustomizedInput
-                    fieldInfo={bookTitle}
-                    onChange={(text) => {
-                        setBookTitle({ ...bookTitle, value: text, error: "" });
-                    }}
-                    style={styles.titleStyle}
-                />
+        <View style={styles.buttonContainer}>
+          <Button
+            text={isEdited ? 'Edit Changes' : 'Save Changes'}
+            style={{
+              marginTop: UtilityMethods.hp(4),
+            }}
+            onPress={() => {}}
+          />
+          <Button
+            text={'Discard'}
+            style={styles.changePassowrd}
+            textStyle={styles.changePassowrdText}
+            onPress={handleDiscard}
+          />
+        </View>
+      </ScreenWrapper>
+    </MainLayout>
+  );
+};
 
-                <AssetsUploaderComponent
-                    assets={assets}
-                    setAssets={setAssets}
-                />
-
-
-
-                <View style={styles.buttonContainer}>
-                    <Button
-                        text={isEdited? "Edit Changes" : "Save Changes"}
-                        style={{
-                            marginTop: UtilityMethods.hp(4)
-                        }}
-                        onPress={() => {}}
-                    />
-                    <Button
-                        text={"Discard"}
-                        style={styles.changePassowrd}
-                        textStyle={styles.changePassowrdText}
-                        onPress={handleDiscard}
-                    />
-                </View>
-
-            </ScreenWrapper>
-        </MainLayout>
-    )
-}
-
-export default AddNewBook
+export default AddNewBook;
--- src/utility/cropImagePicker.js
+++ src/utility/cropImagePicker.js
@@ -1,3 +1,4 @@
+import {Platform} from 'react-native';
 import ImagePicker from 'react-native-image-crop-picker';
 
 const pickImage = async (multiple = false) => {
@@ -5,30 +6,41 @@ const pickImage = async (multiple = false) => {
     if (multiple) {
       const assets = await ImagePicker.openPicker({
         multiple: true,
-        maxFiles:30,
+        maxFiles: 30,
         mediaType: 'any',
       });
-      return assets.map(asset => ({
-        uri: asset.path,
-        width: asset.width,
-        height: asset.height,
-        mime: asset.mime,
-      }));
+      return assets.map(asset => formateData(asset));
     } else {
       const asset = await ImagePicker.openPicker({
         mediaType: 'photo',
-        cropping:true
+        cropping: true,
       });
-      return {
-        uri: asset.path,
-        width: asset.width,
-        height: asset.height,
-        mime: asset.mime,
-      };
+      return formateData(asset);
     }
   } catch (error) {
     throw error;
   }
+};
+
+const formateData = response => {
+  return Platform.OS === 'android'
+    ? {
+        uri: response.path,
+        name: getFileName(response.path),
+        type: response.mime,
+        width: response.width,
+        height: response.height,
+      }
+    : {
+        uri: response.path,
+        name: response.filename,
+        type: response.mime,
+        width: response.width,
+        height: response.height,
+      };
+};
+const getFileName = filePath => {
+  return filePath.split('/').pop();
 };
 
 export default pickImage;
